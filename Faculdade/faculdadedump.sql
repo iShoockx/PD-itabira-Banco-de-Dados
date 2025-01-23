2c80420d3fb9e317e7819c4ae3b11b3074b9b20e
iShoockx <160795305+iShoockx@users.noreply.github.com>
2025-01-23 07:55:10 -0300

    Faculdadedump

---
+++ Faculdade/faculdadedump.sql
@@ -0,0 +1,243 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: faculdade
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno` (
+  `nome` varchar(255) NOT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  `matricula` varchar(10) NOT NULL,
+  `endereço` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aluno`
+--
+
+LOCK TABLES `aluno` WRITE;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+INSERT INTO `aluno` VALUES ('Livia Silva','1998-02-01','1234567890','Rua A, 42'),('Dorothy Vaughan','1989-03-19','2234567890','Rua B, 6'),('Nina Silva','1983-12-31','3334567890','Rua C, 190'),('Brace Hopper','1982-10-20','4444567890','Rua D, 78'),('Margaret Hamilton','1992-10-28','5555567890','Rua E,87');
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aluno_disciplina`
+--
+
+DROP TABLE IF EXISTS `aluno_disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno_disciplina` (
+  `matricula` varchar(10) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`matricula`,`nome`),
+  KEY `nome` (`nome`),
+  CONSTRAINT `aluno_disciplina_ibfk_1` FOREIGN KEY (`matricula`) REFERENCES `aluno` (`matricula`),
+  CONSTRAINT `aluno_disciplina_ibfk_2` FOREIGN KEY (`nome`) REFERENCES `disciplina` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aluno_disciplina`
+--
+
+LOCK TABLES `aluno_disciplina` WRITE;
+/*!40000 ALTER TABLE `aluno_disciplina` DISABLE KEYS */;
+INSERT INTO `aluno_disciplina` VALUES ('1234567890','Algoritmos e Estruturas de Dados'),('2234567890','Algoritmos e Estruturas de Dados'),('3334567890','Algoritmos e Estruturas de Dados'),('4444567890','Algoritmos e Estruturas de Dados'),('5555567890','Algoritmos e Estruturas de Dados'),('1234567890','Introdução a Banco de dados'),('2234567890','Introdução a Banco de dados'),('3334567890','Introdução a Banco de dados'),('4444567890','Introdução a Banco de dados'),('5555567890','Introdução a Banco de dados'),('1234567890','Introdução a Programação'),('2234567890','Introdução a Programação'),('3334567890','Introdução a Programação'),('4444567890','Introdução a Programação'),('5555567890','Introdução a Programação');
+/*!40000 ALTER TABLE `aluno_disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `avaliaçao`
+--
+
+DROP TABLE IF EXISTS `avaliaçao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avaliaçao` (
+  `prof_cpf` varchar(11) NOT NULL,
+  `data_hora` datetime NOT NULL,
+  `comentario` varchar(500) DEFAULT NULL,
+  `nota` int DEFAULT NULL,
+  PRIMARY KEY (`prof_cpf`,`data_hora`),
+  CONSTRAINT `avaliaçao_ibfk_1` FOREIGN KEY (`prof_cpf`) REFERENCES `professor` (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avaliaçao`
+--
+
+LOCK TABLES `avaliaçao` WRITE;
+/*!40000 ALTER TABLE `avaliaçao` DISABLE KEYS */;
+INSERT INTO `avaliaçao` VALUES ('09876543212','2024-03-10 10:01:00','otima didatica',10),('09876543212','2024-05-04 10:01:01','muito prestativa',9);
+/*!40000 ALTER TABLE `avaliaçao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `departamento`
+--
+
+DROP TABLE IF EXISTS `departamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `departamento` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `local` varchar(255) DEFAULT NULL,
+  `prof_chefe_cpf` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `prof_chefe_cpf` (`prof_chefe_cpf`),
+  CONSTRAINT `departamento_ibfk_1` FOREIGN KEY (`prof_chefe_cpf`) REFERENCES `professor` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departamento`
+--
+
+LOCK TABLES `departamento` WRITE;
+/*!40000 ALTER TABLE `departamento` DISABLE KEYS */;
+INSERT INTO `departamento` VALUES (1,'Ciencias Exatas','Belo Horizonte','12345678901');
+/*!40000 ALTER TABLE `departamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `disciplina`
+--
+
+DROP TABLE IF EXISTS `disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `disciplina` (
+  `nome` varchar(100) NOT NULL,
+  `carga_horaria` int NOT NULL DEFAULT '30',
+  `ementa` text,
+  `disc_pre_requisito` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`nome`),
+  KEY `disc_pre_requisito` (`disc_pre_requisito`),
+  CONSTRAINT `disciplina_ibfk_1` FOREIGN KEY (`disc_pre_requisito`) REFERENCES `disciplina` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `disciplina`
+--
+
+LOCK TABLES `disciplina` WRITE;
+/*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
+INSERT INTO `disciplina` VALUES ('Algoritmos e Estruturas de Dados',68,'F,G,H,I','Introdução a programação'),('Introdução a Banco de dados',45,'X,Y,W,Z',NULL),('Introdução a Programação',38,'A,B,C,D',NULL);
+/*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `professor`
+--
+
+DROP TABLE IF EXISTS `professor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professor` (
+  `inicio_contrato` date DEFAULT NULL,
+  `nome` varchar(255) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `depto_id` int DEFAULT NULL,
+  PRIMARY KEY (`cpf`),
+  KEY `depto_id` (`depto_id`),
+  CONSTRAINT `professor_ibfk_1` FOREIGN KEY (`depto_id`) REFERENCES `departamento` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `professor`
+--
+
+LOCK TABLES `professor` WRITE;
+/*!40000 ALTER TABLE `professor` DISABLE KEYS */;
+INSERT INTO `professor` VALUES ('2024-01-02','ada lovelace','09876543212',NULL),('2024-02-10','Hedy Lamarr','12345678901',NULL);
+/*!40000 ALTER TABLE `professor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `professor_contato`
+--
+
+DROP TABLE IF EXISTS `professor_contato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professor_contato` (
+  `prof_cpf` varchar(11) NOT NULL,
+  `contato` varchar(14) NOT NULL,
+  PRIMARY KEY (`prof_cpf`,`contato`),
+  CONSTRAINT `professor_contato_ibfk_1` FOREIGN KEY (`prof_cpf`) REFERENCES `professor` (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `professor_contato`
+--
+
+LOCK TABLES `professor_contato` WRITE;
+/*!40000 ALTER TABLE `professor_contato` DISABLE KEYS */;
+INSERT INTO `professor_contato` VALUES ('09876543212','319876543'),('12345678901','219876543');
+/*!40000 ALTER TABLE `professor_contato` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `professor_disciplina`
+--
+
+DROP TABLE IF EXISTS `professor_disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professor_disciplina` (
+  `cpf` varchar(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`cpf`,`nome`),
+  KEY `nome` (`nome`),
+  CONSTRAINT `professor_disciplina_ibfk_1` FOREIGN KEY (`cpf`) REFERENCES `professor` (`cpf`),
+  CONSTRAINT `professor_disciplina_ibfk_2` FOREIGN KEY (`nome`) REFERENCES `disciplina` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `professor_disciplina`
+--
+
+LOCK TABLES `professor_disciplina` WRITE;
+/*!40000 ALTER TABLE `professor_disciplina` DISABLE KEYS */;
+INSERT INTO `professor_disciplina` VALUES ('09876543212','Algoritmos e Estruturas de Dados'),('12345678901','Introdução a Banco de dados'),('12345678901','Introdução a Programação');
+/*!40000 ALTER TABLE `professor_disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-23  7:49:25
